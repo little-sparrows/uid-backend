96b990091ccdadd7c456c3a632a62f2fa4f907d8
Shapovalov Mihail <mihailsapovalov05@gmail.com>
2023-04-28 22:01:03 +0300

    Abstract from exact session type in routers

--- routers/common.py
+++ routers/common.py
@@ -1,8 +1,14 @@
+from typing_extensions import TypeAlias
+from typing import AsyncGenerator
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.database import engine
 
 
-async def database_session():
+SessionType: TypeAlias = AsyncSession
+
+
+async def database_session() -> AsyncGenerator[SessionType, None, None]:
     async with AsyncSession(engine, expire_on_commit=False) as session:
         yield session
